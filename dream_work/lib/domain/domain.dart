d7b9a427fa61d05eeafe4243544f67d36a8a8020
Terry <terry@luobo.ca>
2022-03-27 05:29:39 -0300

    CHANGE: add mixin method for Fetcher class

--- dream_work/lib/domain/domain.dart
+++ dream_work/lib/domain/domain.dart
@@ -1,2 +1,1 @@
 export 'fetcher/fetcher.dart';
-export 'model/model.dart';
--- dream_work/lib/domain/fetcher/individual_fetcher.dart
+++ dream_work/lib/domain/fetcher/individual_fetcher.dart
@@ -1,27 +1,23 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
-import '../model/model.dart';
 
-// todo remove those when everything ready
-const String _demoUrl = 'http://localhost:3000';
-const String _demoAuthToken =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxIiwibmFtZSI6InRlc3QxIiwiZW1haWwiOiJ0ZXN0MSIsImlhdCI6MTY0NzQ3MzM3OH0.xA-RbK5JdXqANTqNZeoWsPWxHqohyIroImASATLeEHY';
-
-class IndividualFetcher {
-  IndividualFetcher({
-    this.authToken = _demoAuthToken, // jwt auth token
-    this.serverUrl = _demoUrl, // server url
-  }); // todo remove the default values when everything ready
-
-  final String authToken;
+abstract class Fetcher {
+  final String? authToken;
   final String serverUrl;
-  final client = http.Client();
+  final http.Client client = http.Client();
 
-  ///----------------------------------------------------------------------------
-  /// Fetches the all data belongs to the user, user data will in the authToken.
-  ///----------------------------------------------------------------------------
-  Future<List<Map<dynamic, dynamic>>?> fetchAll() async {
+  Fetcher({
+    this.authToken,
+    required this.serverUrl,
+  });
+}
+
+mixin Individual<T, E> on Fetcher {
+  Future<List<Map<T, E>>?> fetch({
+    required String authToken,
+    required String serverUrl,
+    required http.Client client,
+  }) async {
     final response = await client.get(
       Uri.parse('$serverUrl/api/todos'),
       headers: {
@@ -29,49 +25,34 @@ class IndividualFetcher {
         'Authorization': authToken,
       },
     );
-
-    if (response.statusCode == 200) {
-      final List<dynamic> rawData = json.decode(response.body);
-      final List<Map<dynamic, dynamic>> data =
-          rawData.map((e) => Map.castFrom(e)).toList();
-      return data;
+    //assert(response.statusCode != 200, response.body);
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load post');
     }
-    assert(response.statusCode != 200, response.body);
-    return null;
+
+    print(response.body);
+
+    final List<dynamic> rawData = json.decode(response.body);
+    final List<Map<T, E>> data =
+        rawData.map((e) => Map.castFrom(e) as Map<T, E>).toList();
+    return data;
   }
+}
 
-  ///----------------------------------------------------------------------------
-  /// fetche a single item by a tid or a uid, not both.
-  /// tid: task id
-  /// pid: project id
-  ///----------------------------------------------------------------------------
-  Future<Map<dynamic, dynamic>?> fetchById({
-    String? tid,
-    String? pid,
-  }) async {
-    assert(!(tid == null && pid == null), 'one of tid or pid must be provided');
-    assert(!(tid != null && pid != null),
-        'only one of tid or pid should be provided');
+class IndividualFetcher extends Fetcher with Individual<String, dynamic> {
+  IndividualFetcher({
+    required String authToken,
+    required String serverUrl,
+  }) : super(
+          authToken: authToken,
+          serverUrl: serverUrl,
+        );
 
-    if (tid != null) {
-      final response = await client.get(
-        Uri.parse('$serverUrl/api/todos/$tid'),
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': authToken,
-        },
-      );
-
-      if (response.statusCode == 200) {
-        return Map.castFrom(json.decode(response.body));
-      }
-      assert(response.statusCode != 200, response.body);
-    }
-
-    if (pid != null) {
-      // todo also add this to the server
-    }
-
-    return null;
+  void getResult() async {
+    await Future.wait({
+      fetch(authToken: authToken!, serverUrl: serverUrl, client: client)
+          .then((value) => print(value))
+      //.catchError((value) => print(value)),
+    });
   }
 }
