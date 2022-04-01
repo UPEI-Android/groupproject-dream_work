0ff0fc8966e5f12ec4f88c0c12a92b30ae2822c8
Terry <terry@luobo.ca>
2022-04-01 05:20:24 -0300

    UPDATE: add a custom firebase-like lib for user auth with backend

--- dream_work/lib/screens/auth_screen.dart
+++ dream_work/lib/screens/auth_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../dream_connector/dreamConnector.dart';
 
 class AuthScreen extends StatelessWidget {
   const AuthScreen({Key? key}) : super(key: key);
