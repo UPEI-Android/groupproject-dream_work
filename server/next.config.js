560ac945b93734e8e9cf4266c25f7237c631eaaf
Terry <terry@luobo.ca>
2022-04-02 21:55:40 -0300

    update: Dockerfile

--- server/next.config.js
+++ server/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    experimental: {
+        outputStandalone: true,
+    },
 }
 
 module.exports = nextConfig
