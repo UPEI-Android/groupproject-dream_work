2965ee204a361e59e1aeceea5fa488076b673929
Terry <terry@luobo.ca>
2022-04-03 09:54:17 -0300

    remove: user auth form from profiletab

--- dream_work/lib/widgets/tabs/profiletab.dart
+++ dream_work/lib/widgets/tabs/profiletab.dart
@@ -11,8 +11,6 @@ class ProfileTab extends StatefulWidget {
 
 class _ProfileTabState extends State<ProfileTab> {
   final BorderRadiusGeometry borderRadius = BorderRadius.circular(20);
-  late final TextEditingController _email;
-  late final TextEditingController _password;
 
   double _height = 80;
   bool _isExpanded = false;
@@ -29,32 +27,6 @@ class _ProfileTabState extends State<ProfileTab> {
     setState(() {
       _error = error;
     });
-  }
-
-  void _setExpanded(bool isExpanded) {
-    setState(() {
-      _isExpanded = isExpanded;
-    });
-  }
-
-  void _setHeight([double height = 100]) {
-    setState(() {
-      _height = height;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _email = TextEditingController();
-    _password = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
   }
 
   @override
@@ -91,21 +63,11 @@ class _ProfileTabState extends State<ProfileTab> {
           stream: DreamAuth.instance.authState,
           builder: (BuildContext context, AsyncSnapshot snap) {
             return snap.data == null
-                ? _isExpanded
-                    ? _authForm()
-                    : ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.all(20),
-                        ),
-                        onPressed: () async {
-                          _setHeight(280);
-                          await Future.delayed(
-                              const Duration(milliseconds: 200), () {
-                            _setExpanded(!_isExpanded);
-                          });
-                        },
-                        child: const Text('Login / Register',
-                            style: TextStyle(fontSize: 25.0)))
+                ? const Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.greenAccent,
+                    ),
+                  )
                 : Profile(
                     userEmail: snap.data['email'] ?? '',
                     userName: snap.data['name'] ?? '',
@@ -114,114 +76,6 @@ class _ProfileTabState extends State<ProfileTab> {
           },
         ),
       );
-
-  Widget _authForm() => Form(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: borderRadius,
-            color: Colors.greenAccent,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(_error ?? '',
-                    style: const TextStyle(
-                        color: Colors.red, fontWeight: FontWeight.bold)),
-                TextFormField(
-                  controller: _email,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                    icon: Icon(Icons.person),
-                    border: OutlineInputBorder(),
-                    labelText: 'Username / Email',
-                  ),
-                ),
-                TextFormField(
-                  controller: _password,
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  keyboardType: TextInputType.visiblePassword,
-                  decoration: const InputDecoration(
-                    icon: Icon(Icons.lock),
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  ),
-                ),
-                _error == 'No such user' ? _registerBtn() : _signinBtn(),
-              ],
-            ),
-          ),
-        ),
-      );
-
-  Widget _signinBtn() => ElevatedButton(
-        child: Text(
-          _isLoading ? 'loading...' : 'Login',
-        ),
-        onPressed: () async {
-          _setLoading(true);
-          _signin();
-          _setLoading(false);
-        },
-      );
-
-  Widget _registerBtn() => ElevatedButton(
-        child: Text(
-          _isLoading ? 'loading...' : 'Resgister',
-        ),
-        onPressed: () async {
-          _setLoading(true);
-          _register();
-          _setLoading(false);
-        },
-      );
-
-  Future<void> _signin() async {
-    final String email = _email.text;
-    final String password = _password.text;
-    await DreamAuth.instance
-        .loginWithEmailAndPassword(
-      email: email,
-      password: password,
-    )
-        .then(
-      (value) {
-        _restTag();
-      },
-    ).catchError(
-      (e) {
-        _setError(e.toString().split(': ').last);
-      },
-    );
-  }
-
-  Future<void> _register() async {
-    final String email = _email.text;
-    final String password = _password.text;
-    await DreamAuth.instance
-        .createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    )
-        .then(
-      (value) {
-        _restTag();
-      },
-    ).catchError(
-      (e) {
-        _setError(e.toString().split(': ').last);
-      },
-    );
-  }
-
-  void _restTag() {
-    _setExpanded(false);
-    _setHeight();
-    _setError();
-  }
 }
 
 Widget _divider() => const Divider(
